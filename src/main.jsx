0d9240cdb24dc6df9657c5dcc45005a1733ed7ca
Tu Mejor Versión <tumejorversionbyivy@gmail.com>
2026-02-06 19:55:45 -0300

    fix spa routing for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/main.css'
 
+const redirect = sessionStorage.redirect
+delete sessionStorage.redirect
+
+if (redirect && redirect !== '/') {
+  window.history.replaceState(null, null, redirect)
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
