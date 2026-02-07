7d226a8ee1d1a9520b667e75965be4dcd0c0d889
Tu Mejor Versión <tumejorversionbyivy@gmail.com>
2026-02-07 18:41:43 -0300

    fix: correct paths for production build

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './styles/main.css'
+import "./styles/main.css";
+
 
 const redirect = sessionStorage.redirect
 delete sessionStorage.redirect
